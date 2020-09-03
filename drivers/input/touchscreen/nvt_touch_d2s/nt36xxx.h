41bf3c438bb07814e6075518af559edbc6b1f17d
Manish4586 <manish.n.manish45@gmail.com>
2020-09-04 05:09:30 +0530

    touchscreen: nvt_d2s: disable esd and mpctrlram
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.h
@@ -68,7 +68,6 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
-#define NVT_TOUCH_MP 1
 #define MT_PROTOCOL_B 1
 #define WAKEUP_GESTURE 1
 #if WAKEUP_GESTURE
@@ -77,10 +76,6 @@ extern const uint16_t gesture_key_array[];
 #define BOOT_UPDATE_FIRMWARE 1
 #define BOOT_UPDATE_FIRMWARE_NAME_TIANMA "novatek/tianma_nt36672_miui_d2s.bin"
 #define BOOT_UPDATE_FIRMWARE_NAME_JDI "novatek/jdi_nt36672_miui_d2s.bin"
-
-
-#define NVT_TOUCH_ESD_PROTECT 1
-#define NVT_TOUCH_ESD_CHECK_PERIOD 1500	/* ms */
 
 struct nvt_ts_mem_map {
 	uint32_t EVENT_BUF_ADDR;
@@ -174,8 +169,5 @@ extern int32_t nvt_check_fw_reset_state(RST_COMPLETE_STATE check_reset_state);
 extern int32_t nvt_get_fw_info(void);
 extern int32_t nvt_clear_fw_status(void);
 extern int32_t nvt_check_fw_status(void);
-#if NVT_TOUCH_ESD_PROTECT
-extern void nvt_esd_check_enable(uint8_t enable);
-#endif /* #if NVT_TOUCH_ESD_PROTECT */
 
 #endif /* _LINUX_NVT_TOUCH_H */
