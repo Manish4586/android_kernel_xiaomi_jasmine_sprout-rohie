e579ebe457c017571b13f5bf13c2b12879c56d05
Manish4586 <manish.n.manish45@gmail.com>
2020-09-04 05:39:12 +0530

    drivers: touchscreen: nvt: Disable shits
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.h
@@ -63,13 +63,13 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #define TOUCH_FORCE_NUM 1000
 
 
-#define NVT_TOUCH_PROC 1
-#define NVT_TOUCH_EXT_PROC 1
+#define NVT_TOUCH_PROC 0
+#define NVT_TOUCH_EXT_PROC 0
 #define WAKEUP_GESTURE 1
 #if WAKEUP_GESTURE
 extern const uint16_t gesture_key_array[];
 #endif
-#define BOOT_UPDATE_FIRMWARE 1
+#define BOOT_UPDATE_FIRMWARE 0
 #define BOOT_UPDATE_FIRMWARE_NAME_TIANMA "novatek/tianma_nt36672_miui_d2s.bin"
 #define BOOT_UPDATE_FIRMWARE_NAME_JDI "novatek/jdi_nt36672_miui_d2s.bin"
 
