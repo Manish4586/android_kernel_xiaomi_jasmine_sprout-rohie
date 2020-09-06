640c7421f0e3d2718b63e5b394bd473a277175d0
Manish4586 <manish.n.manish45@gmail.com>
2020-09-06 18:57:12 +0530

    tcp: remove unsigned integer
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -220,7 +220,6 @@ struct tcp_sock {
 		u8 reord;    /* reordering detected */
 	} rack;
 	u16	advmss;		/* Advertised MSS			*/
-	u8	unused;
 	u32	chrono_start;	/* Start time in jiffies of a TCP chrono */
 	u32	chrono_stat[3];	/* Time in jiffies for chrono_stat stats */
 	u8	chrono_type:2,	/* current chronograph type */
